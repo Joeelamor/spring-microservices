d05d9f72a4043f5700d1ddc6e330bb0b7b2baee3
Joeelamor <lizhongzhang330@gmail.com>
2019-03-21 11:12:17 -0700

    Add a new project named restful-web-services

---
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,21 @@
+package com.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    public String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
